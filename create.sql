a02774b24f6293e2c14f8e353cd91058e0442d7e
Alvaro1668 <95989290+Alvaro1668@users.noreply.github.com>
2023-09-06 16:08:58 -0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE cadastro_usuarios;
 
+USE cadastro_usuarios;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
