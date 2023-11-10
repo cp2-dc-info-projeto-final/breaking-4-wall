64f669799d292907f3f0de2ee3f0b9551376aadf
Alvaro1668 <95989290+Alvaro1668@users.noreply.github.com>
2023-11-10 11:05:25 -0300

    Update login.php

--- src/login.php
+++ src/login.php
@@ -1,40 +1,66 @@
 <?php
 $message = "";
 
-$servername = "localhost"; // Endereço do servidor de banco de dados
-$username_db = "cadastrado"; // Nome de usuário do banco de dados
-$password_db = "123"; // Senha do banco de dados
-$database = "CADASTRO"; // Nome do banco de dados
+$servername = "localhost";
+$username_db = "cadastrado";
+$password_db = "123";
+$database = "CADASTRO";
 
-// Crie uma conexão com o banco de dados
 $conn = new mysqli("localhost", "cadastrados", "123", "CADASTRO");
 
-// Verifique se ocorreu algum erro na conexão
 if ($conn->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-// Incluir código para processar o envio do formulário de login
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $senha = $_POST["senha"];
 
-    // Verificar as credenciais do usuário
     $stmt = $conn->prepare("SELECT id, senha FROM cadastrados WHERE email = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $stmt->bind_result($id, $hashed_password);
     $stmt->fetch();
-    
+
     if (password_verify($senha, $hashed_password)) {
         header("location: index.html");
     } else {
-        $message = "Credenciais inválidas. Tente novamente.";
+        $message = "Senha incorreta. Tente novamente.";
     }
-    
+
     $stmt->close();
 }
 
-// Feche a conexão com o banco de dados quando não precisar mais dela
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+
+    <div class="container">
+        <form action="login.php" method="POST" class="registration-form">
+            <h2>Login</h2>
+
+            <?php if (!empty($message)) : ?>
+                <p style="color: red;"><?php echo $message; ?></p>
+            <?php endif; ?>
+
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="senha">Senha</label>
+            <input type="password" id="senha" name="senha" required>
+
+            <button type="submit" class="custom-button">Entrar</button>
+        </form>
+    </div>
+
+</body>
+</html>
