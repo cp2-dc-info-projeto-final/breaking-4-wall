7dd976c210882f94421ef104b9f3cb4111f512ad
Alvaro1668 <95989290+Alvaro1668@users.noreply.github.com>
2023-11-11 23:27:30 -0300

    Update processar_cadastro.php

--- src/processar_cadastro.php
+++ src/processar_cadastro.php
@@ -1,49 +1,43 @@
 <?php
-// Inclua o código de conexão com o banco de dados
-require_once("conexao.php");
+// Habilitar a exibição de erros
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Verifique se o formulário foi enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recupere os dados do formulário
-    $titulo = $_POST["titulo"];
-    $ano_lancamento = $_POST["ano_lancamento"];
-    $diretor = $_POST["diretor"];
-    $sinopse = $_POST["sinopse"];
+$servername = "localhost";
+$username = "root"; // seu nome de usuário do banco de dados
+$password = "12345"; // sua senha do banco de dados
+$dbname = "cadastro";
 
-    // Valide os dados (você pode adicionar mais validações conforme necessário)
-    if (empty($titulo)) {
-        echo "O campo Título é obrigatório.";
-    } else {
-        // Prepare a instrução SQL para inserção dos dados na tabela Filmes
-        $sql = "INSERT INTO Filmes (Titulo, AnoLancamento, Diretor, Sinopse) VALUES (?, ?, ?, ?)";
+// Criar conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        // Crie uma declaração preparada
-        $stmt = $conexao->prepare($sql);
-
-        // Verifique se a preparação foi bem-sucedida
-        if ($stmt) {
-            // Vincule os parâmetros e execute a inserção
-            $stmt->bind_param("siss", $titulo, $ano_lancamento, $diretor, $sinopse);
-
-            if ($stmt->execute()) {
-                echo "Filme cadastrado com sucesso!";
-            } else {
-                echo "Erro ao cadastrar o filme: " . $stmt->error;
-            }
-
-            // Feche a declaração preparada
-            $stmt->close();
-        } else {
-            echo "Erro na preparação da declaração SQL: " . $conexao->error;
-        }
-    }
-} else {
-    // Redirecione para a página de cadastro se o formulário não foi enviado
-    header("Location: cadastro_filmes.html");
-    exit();
+// Verificar conexão
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
-// Feche a conexão com o banco de dados
-$conexao->close();
-?>
+// Verifique se o método POST foi usado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Receber dados do formulário
+    $titulo = $_POST['titulo'];
+    $anoLancamento = $_POST['anoLancamento'];
+    $diretor = $_POST['diretor'];
+    $sinopse = $_POST['sinopse'];
 
+    // Preparar o comando SQL
+    $stmt = $conn->prepare("INSERT INTO Filmes (Titulo, AnoLancamento, Diretor, Sinopse) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("siss", $titulo, $anoLancamento, $diretor, $sinopse);
+
+    // Executar o comando SQL
+    if ($stmt->execute()) {
+        echo "Novo registro criado com sucesso";
+    } else {
+        echo "Erro: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?> 
