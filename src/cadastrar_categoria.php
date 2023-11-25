6a2fe7c13603b5332f5cf3dc06921aa863523c2b
Alvaro1668 <95989290+Alvaro1668@users.noreply.github.com>
2023-11-25 19:21:36 -0300

    Create cadastrar_categoria.php

---
+++ src/cadastrar_categoria.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "categorais";
+$password = "123456";
+$dbname = "CADASTRO";
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Checa a conexão
+if ($conn->connect_error) {
+  die("Falha na conexão: " . $conn->connect_error);
+}
+
+// Prepara e vincula
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nomeCategoria = $_POST["nomeCategoria"];
+    
+    $stmt = $conn->prepare("INSERT INTO Categorias (Nome) VALUES (?)");
+    $stmt->bind_param("s", $nomeCategoria);
+
+    // Executa e verifica
+    if ($stmt->execute()) {
+        echo "Nova categoria cadastrada com sucesso!";
+    } else {
+        echo "Erro: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
