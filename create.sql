3534be83889d1f3612dce9da5594dd00c1bf4ec3
jpcp2 <142441842+jpcp2@users.noreply.github.com>
2023-11-27 13:31:34 -0300

    create.sql

--- create.sql
+++ create.sql
@@ -109,15 +109,6 @@ CREATE TABLE cadastrados (
 -------------------------------conexao--------------------------------------------
 
 
-USE CADASTRO; -- Certifique-se de usar o banco de dados correto
-
--- Crie uma tabela para armazenar informações de login
-CREATE TABLE login (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    senha VARCHAR(255) NOT NULL
-);
------------------------------login-------------------------------------------------
 
 
 CREATE DATABASE IF NOT EXISTS CADASTRO;
