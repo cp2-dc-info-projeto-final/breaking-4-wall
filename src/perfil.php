d7c3125b7b0a6a24866c6b130d712336e5f0d2af
Alvaro1668 <95989290+Alvaro1668@users.noreply.github.com>
2023-11-27 23:04:28 -0300

    Create perfil.php

---
+++ src/perfil.php
@@ -0,0 +1,31 @@
+<?php
+session_start(); // Inicia a sessão
+
+// Verifica se o usuário está logado, senão redireciona para a página de login
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: login.php");
+    exit;
+}
+
+// Dados do usuário para exibir no perfil
+$nome = $_SESSION["nome"];
+$email = $_SESSION["email"];
+// Outros dados podem ser adicionados aqui conforme necessário
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Perfil</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Perfil</h2>
+        <p>Nome: <?php echo $nome; ?></p>
+        <p>Email: <?php echo $email; ?></p>
+        <a href="logout.php">Sair da conta</a>
+    </div>
+</body>
+</html>
