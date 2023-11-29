c16bc63a61768b0cae9538665b68d915debc5fb9
jpcp2 <142441842+jpcp2@users.noreply.github.com>
2023-11-29 02:47:05 -0300

    Create conecta.php

---
+++ src/conecta.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "cadastrado"; // Seu nome de usuário do banco de dados
+$password = "123"; // Sua senha do banco de dados
+$dbname = "CADASTRO"; // Nome do seu banco de dados
+
+// Criar a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+?>
