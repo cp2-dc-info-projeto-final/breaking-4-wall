3c45facf5af05a4c571ce50daa7d7723525a1a64
jpcp2 <142441842+jpcp2@users.noreply.github.com>
2023-11-29 02:49:40 -0300

    Create excluir_admin.php

---
+++ src/excluir_admin.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+require_once 'conecta.php';
+
+// Verifica se o usuário está logado e se o ID foi passado
+if (!isset($_GET['id'], $_SESSION['admin_id']) || $_GET['id'] != $_SESSION['admin_id']) {
+    // Redireciona para a página de login ou exibe uma mensagem de erro
+    exit('Operação não permitida.');
+}
+
+$adminId = $_GET['id'];
+
+// Inicia uma transação para garantir a integridade dos dados
+$conexao->begin_transaction();
+
+try {
+    // Prepara a consulta SQL para excluir o administrador
+    $sql = "DELETE FROM Administradores WHERE id = ?";
+    $stmt = $conexao->prepare($sql);
+    if ($stmt === false) {
+        throw new Exception('Não foi possível preparar a declaração: ' . $conexao->error);
+    }
+
+    // Vincula o parâmetro e executa a consulta
+    $stmt->bind_param('i', $adminId);
+    $stmt->execute();
+    
+    // Verifica se a exclusão foi bem-sucedida
+    if ($stmt->affected_rows > 0) {
+        echo "Administrador excluído com sucesso.";
+        // Se você estiver usando sessões para armazenar informações do administrador, limpe-as aqui
+        unset($_SESSION['admin_id']);
+        // Commit da transação
+        $conexao->commit();
+        // Redireciona para a página de login ou para a lista de administradores
+        header('Location: login_adm.php');
+        exit;
+    } else {
+        throw new Exception('Nenhum administrador foi excluído.');
+    }
+} catch (Exception $e) {
+    // Se ocorrer algum erro, desfaz as alterações
+    $conexao->rollback();
+    exit('Erro ao excluir administrador: ' . $e->getMessage());
+} finally {
+    // Fecha a declaração e a conexão
+    $stmt->close();
+    $conexao->close();
+}
+?>
