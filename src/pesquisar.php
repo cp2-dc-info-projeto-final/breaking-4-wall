b2390bace5e2f32931821ea7eb49847c1d7a91d0
Alvaro1668 <95989290+Alvaro1668@users.noreply.github.com>
2023-11-30 01:56:44 -0300

    Update pesquisar.php

--- src/pesquisar.php
+++ src/pesquisar.php
@@ -1,29 +1,40 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["search_query"])) {
-    // Obtenha o termo de pesquisa da URL
-    $searchQuery = $_GET["search_query"];
+// Configuração das variáveis de conexão com o banco de dados
+$servername = "localhost";
+$username = "alvaro";
+$password = "12345";
+$dbname = "cadastro";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Aqui você pode realizar a consulta no banco de dados para buscar os resultados da pesquisa
-    // Substitua este exemplo com a lógica real da consulta
-
-    // Exemplo de consulta fictícia
-    $resultados = array();
-    // Consulta fictícia no banco de dados usando $searchQuery e armazena os resultados em $resultados
-
-    // Agora você pode exibir os resultados para o usuário
-    echo "<h2>Resultados da pesquisa para: " . htmlspecialchars($searchQuery) . "</h2>";
-    if (empty($resultados)) {
-        echo "<p>Nenhum resultado encontrado.</p>";
-    } else {
-        echo "<ul>";
-        foreach ($resultados as $resultado) {
-            echo "<li>" . htmlspecialchars($resultado) . "</li>";
-        }
-        echo "</ul>";
-    }
-} else {
-    // Se o formulário não foi enviado corretamente, redirecione para a página inicial ou outra página
-    header("Location: index.html");
-    exit();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+$searchQuery = $_GET["search_query"];
+$searchTerm = "%" . $conn->real_escape_string($searchQuery) . "%";
+
+$stmt = $conn->prepare("SELECT ID, Titulo FROM Filmes WHERE Titulo LIKE ?");
+$stmt->bind_param("s", $searchTerm);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows == 1) {
+    // Se apenas um filme for encontrado, redirecione para a página de detalhes
+    $row = $result->fetch_assoc();
+    $filmeId = $row['ID'];
+    header("Location: detalhes_filme.php?id=" . $filmeId);
+    exit();
+} elseif ($result->num_rows > 1) {
+    // Se múltiplos filmes forem encontrados, liste-os
+    echo "<h2>Filmes Encontrados:</h2>";
+    echo "<ul>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<li><a href='detalhes_filme.php?id=" . $row['ID'] . "'>" . htmlspecialchars($row['Titulo']) . "</a></li>";
+    }
+    echo "</ul>";
+} else {
+    echo "<p>Filme não encontrado.</p>";
+}
+
+$conn->close();
 ?>
