9c8ec4c2f7ebc29db4a9d2b8d2c65f2980b17bac
Alvaro1668 <95989290+Alvaro1668@users.noreply.github.com>
2023-11-29 23:25:50 -0300

    Update login.php

--- src/login.php
+++ src/login.php
@@ -60,6 +60,92 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Login</title>
+    <style>
+/* Estilo do corpo da página com a nova cor de fundo */
+body {
+    background-color: #561237; /* Cor de fundo atualizada baseada na imagem */
+    font-family: Arial, sans-serif;
+    color: #fff;
+    margin: 0;
+    padding: 0;
+}
+
+/* Estilo do container do formulário de login */
+.container {
+    max-width: 400px;
+    margin: 50px auto;
+    padding: 20px;
+    background-color: #123456; /* Cor de fundo para o container do formulário */
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+/* Estilos para o formulário de login */
+.login-form {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+
+/* Estilo para os rótulos do formulário */
+.login-form label {
+    color: #FFFFFF; /* Cor do texto para contraste com o fundo escuro */
+    margin-bottom: 5px;
+    font-weight: bold;
+}
+
+/* Estilo para os campos de entrada */
+.login-form input[type="email"],
+.login-form input[type="password"] {
+    padding: 10px;
+    border-radius: 4px;
+    border: 1px solid #444; /* Cor de borda ajustada para contraste */
+    background-color: #FFF;
+    color: #000;
+}
+
+/* Estilo para o botão de login */
+.custom-button {
+    background-color: rgb(255, 0, 119); /* Cor do botão atualizada para rosa */
+    color: #FFF;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+.custom-button:hover {
+    background-color: #e6007e; /* Tom mais escuro de rosa para o estado de hover */
+}
+
+/* Mensagem de erro ou sucesso */
+p {
+    background-color: #444; /* Fundo para mensagem */
+    color: #FFF;
+    padding: 10px;
+    border-radius: 4px;
+    text-align: center;
+}
+
+/* Estilo para a mensagem de erro */
+.login-form p {
+    background-color: #ff3860; /* Cor de fundo para mensagens de erro */
+    color: #ffffff;
+    padding: 10px;
+    border-radius: 4px;
+    text-align: center;
+    margin-top: 15px;
+}
+
+/* Estilo para o título do formulário */
+.login-form h2 {
+    color: #FFFFFF; /* Define a cor do título para branco */
+    /* Outros estilos para h2 conforme necessário */
+}
+
+    </style>
+    
 </head>
 <body>
     <div class="container">
