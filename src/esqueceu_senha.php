1c093385ca9e92a711daf4c6938ae6eba3a0361d
Alunocp2jv <142441786+Alunocp2jv@users.noreply.github.com>
2023-12-01 14:47:25 -0300

    Create esqueceu_senha.php

---
+++ src/esqueceu_senha.php
@@ -0,0 +1,28 @@
+<?php
+include "conecta.php";
+session_start();
+?>
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <title>Esqueci a senha | Breaking4Wall</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h2>Recuperação de Senha</h2>
+    <?php
+        if (isset($_SESSION['msg_rec'])) {
+            echo "<p>" . $_SESSION['msg_rec'] . "</p>";
+            unset($_SESSION['msg_rec']);
+        }
+    ?>
+    <form action="recupera.php" method="POST">
+        <input type="hidden" name="operacao" value="enviar-email">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" placeholder="Digite seu email" required>
+        <br>
+        <button type="button" onclick="location.href='login.php'">Voltar</button>
+        <button type="submit">Continuar</button>
+    </form>
+</body>
+</html>
