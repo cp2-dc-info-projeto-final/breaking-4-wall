e15499e6e80fc3a399c65d97ea42d0dffad12320
jpcp2 <142441842+jpcp2@users.noreply.github.com>
2023-12-01 22:30:47 -0300

    Update inserir_comentario.php

--- src/inserir_comentario.php
+++ src/inserir_comentario.php
@@ -2,43 +2,41 @@
 session_start();
 require_once 'conecta.php';
 
-// Verifica se o formulário foi submetido
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Informações de conexão com o banco de dados
-    $servername = "localhost";
-    $username = "cadastrados"; // Seu nome de usuário do banco de dados
-    $password = "123"; // Sua senha do banco de dados
-    $dbname = "CADASTRO";
+    // Conexão com o banco de dados
+    $conn = new mysqli("localhost", "cadastrados", "123", "CADASTRO");
 
-    // Cria uma conexão com o banco de dados
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Verifica se ocorreu algum erro na conexão
+    // Verifica a conexão
     if ($conn->connect_error) {
         die("Erro na conexão com o banco de dados: " . $conn->connect_error);
     }
 
-    $filmeId = $_POST['filme_id'];
-    $comentario = $_POST['comentario'];
-    $usuarioId = isset($_SESSION['usuario_id']) ? $_SESSION['usuario_id'] : null; // Define null se o usuário não estiver logado
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+        // Usuário está logado
+        $filmeId = $_POST['filme_id'];
+        $comentario = $_POST['comentario'];
+        $usuarioId = $_SESSION['id']; // Usando o ID do usuário logado
 
-    // Prepara a consulta SQL para inserir o comentário
-    $stmt = $conn->prepare("INSERT INTO comentarios (filme_id, usuario_id, comentario) VALUES (?, ?, ?)");
-    $stmt->bind_param("iis", $filmeId, $usuarioId, $comentario);
-    $stmt->execute();
+        // Insere o comentário no banco de dados
+        $stmt = $conn->prepare("INSERT INTO comentarios (filme_id, usuario_id, comentario) VALUES (?, ?, ?)");
+        $stmt->bind_param("iis", $filmeId, $usuarioId, $comentario);
+        $stmt->execute();
 
-    if ($stmt->error) {
-        // Tratar erro ao inserir o comentário
-        echo "Erro: " . $stmt->error;
+        if ($stmt->error) {
+            echo "Erro: " . $stmt->error;
+        } else {
+            header("Location: detalhes_filme.php?id=" . $filmeId);
+            exit;
+        }
+        $stmt->close();
     } else {
-        // Redireciona de volta para a página do filme após inserir o comentário
-        header("Location: detalhes_filme.php?id=" . $filmeId);
+        // Se o usuário não estiver logado, exibe uma mensagem
+        echo "<p>Você precisa estar logado para comentar.</p>";
     }
 
-    $stmt->close();
     $conn->close();
 } else {
-    // Redireciona para a página principal se o script for acessado diretamente
     header("Location: index.html");
+    exit;
 }
 ?>
