1b54c7b51de4166c176e2b2c2d5d8101f76835c0
jpcp2 <142441842+jpcp2@users.noreply.github.com>
2023-12-02 21:15:54 -0300

    Update editar_filme.php

--- src/editar_filme.php
+++ src/editar_filme.php
@@ -2,12 +2,6 @@
 session_start();
 require_once 'conecta.php';
 
-// Verifique se o usuário está logado como administrador
-if (!isset($_SESSION['admin_id'])) {
-    header('Location: login_adm.php');
-    exit;
-}
-
 // Garantir que um ID de filme foi passado
 if (!isset($_GET['id'])) {
     echo "ID do filme não fornecido.";
@@ -60,7 +54,80 @@ $conn->close();
 <!DOCTYPE html>
 <html lang="pt">
 <head>
-    <!-- Metadados e links para CSS -->
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            background: #f4f4f4;
+            margin: 0;
+            padding: 20px;
+            line-height: 1.6;
+        }
+
+        .container {
+            max-width: 700px;
+            margin: 30px auto;
+            padding: 20px;
+            background: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+        }
+
+        h2 {
+            color: #333;
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            margin: 10px 0 5px;
+        }
+
+        input[type="text"],
+        input[type="number"],
+        textarea {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 20px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        button {
+            display: block;
+            width: 100%;
+            padding: 10px;
+            background-color: #5C7AEA;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        button:hover {
+            background-color: #4a69bd;
+        }
+
+        /* Estilo de erro para mensagem */
+        .error-message {
+            color: #D8000C;
+            background-color: #FFD2D2;
+            border: 1px solid #D8000C;
+            margin-bottom: 20px;
+            padding: 10px;
+            border-radius: 4px;
+        }
+
+        /* Estilo de sucesso para mensagem */
+        .success-message {
+            color: #4F8A10;
+            background-color: #DFF2BF;
+            border: 1px solid #4F8A10;
+            margin-bottom: 20px;
+            padding: 10px;
+            border-radius: 4px;
+        }
+    </style>
 </head>
 <body>
     <form action="editar_filme.php?id=<?php echo $filmeId; ?>" method="post">
