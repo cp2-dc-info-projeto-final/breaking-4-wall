baf1da1820efadc4557632de199741c4b4a6ac0d
jpcp2 <142441842+jpcp2@users.noreply.github.com>
2023-12-02 21:47:02 -0300

    Update excluir_filme.php

--- src/excluir_filme.php
+++ src/excluir_filme.php
@@ -2,13 +2,7 @@
 session_start();
 require_once 'conecta.php';
 
-// Verifique se o usuário está logado como administrador
-if (!isset($_SESSION['admin_id'])) {
-    header('Location: login_adm.php');
-    exit;
-}
-
-// Garantir que um ID de filme foi passado
+// Garanta que um ID de filme foi passado
 if (!isset($_GET['id'])) {
     echo "ID do filme não fornecido.";
     exit;
@@ -23,9 +17,6 @@ $deleteStmt->bind_param('i', $filmeId);
 
 if ($deleteStmt->execute()) {
     echo "Filme excluído com sucesso!";
-    // Redirecione para a lista de filmes após a exclusão
-    header('Location: lista_filmes.php');
-    exit;
 } else {
     echo "Erro ao excluir filme: " . $deleteStmt->error;
 }
