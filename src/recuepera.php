2997f028ebb46f1234dc5b605f72b733d2ac8875
Alunocp2jv <142441786+Alunocp2jv@users.noreply.github.com>
2023-12-11 13:31:46 -0300

    Update recuepera.php

--- src/recuepera.php
+++ src/recuepera.php
@@ -1,40 +1,57 @@
 <?php
 include "enviar_email.php";
-include "conecta.php";
+include_once "conecta.php";
 session_start();
 
 $email = $_POST['email'];
 
+// Verifica se a conexão com o banco de dados foi estabelecida corretamente
+if ($conn === null) {
+    die("Erro na conexão com o banco de dados.");
+}
+
 // Função para gerar nova senha e enviar e-mail
-function processarRecuperacaoSenha($mysqli, $email) {
+function processarRecuperacaoSenha($conn, $email) {
     // Gera uma nova senha aleatória
     $nova_senha = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 8);
 
     // Atualiza a senha no banco de dados e envia o e-mail
-    return atualizarSenhaEnviarEmail($mysqli, $email, $nova_senha);
+    return atualizarSenhaEnviarEmail($conn, $email, $nova_senha);
 }
 
 // Função para atualizar a senha no banco de dados e enviar e-mail
-function atualizarSenhaEnviarEmail($mysqli, $email, $nova_senha) {
+function atualizarSenhaEnviarEmail($conn, $email, $nova_senha) {
     $senha_hash = password_hash($nova_senha, PASSWORD_DEFAULT);
     $update_query = "UPDATE cadastrados SET senha = ? WHERE email = ?";
-    $update_stmt = $mysqli->prepare($update_query);
+    $update_stmt = $conn->prepare($update_query);
+
+    // Verifica se a preparação da consulta foi bem-sucedida
+    if ($update_stmt === false) {
+        return "Erro ao preparar a consulta de atualização.";
+    }
+
     $update_stmt->bind_param("ss", $senha_hash, $email);
     $update_stmt->execute();
 
-    if(envia_email($email, "Nova Senha Temporária", "Sua nova senha temporária é: $nova_senha")) {
-        $mysqli->commit();
+    if (envia_email($email, "Nova Senha Temporária", "Sua nova senha temporária é: $nova_senha")) {
+        $conn->commit();
         return "Uma nova senha foi enviada para o seu e-mail.";
     } else {
-        $mysqli->rollback();
+        $conn->rollback();
         return "Falha ao enviar e-mail.";
     }
 }
 
 // Verifica se o e-mail existe e processa a recuperação de senha
-function verificarEmail($mysqli, $email) {
+function verificarEmail($conn, $email) {
     $query = "SELECT email FROM cadastrados WHERE email = ?";
-    $stmt = $mysqli->prepare($query);
+    $stmt = $conn->prepare($query);
+
+    // Verifica se a preparação da consulta foi bem-sucedida
+    if ($stmt === false) {
+        return "Erro ao preparar a consulta de verificação de e-mail.";
+    }
+
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $result = $stmt->get_result();
@@ -42,18 +59,17 @@ function verificarEmail($mysqli, $email) {
     if ($result->num_rows === 0) {
         return "E-mail não encontrado no banco de dados.";
     } else {
-        $mysqli->begin_transaction();
+        $conn->begin_transaction();
         try {
-            return processarRecuperacaoSenha($mysqli, $email);
+            return processarRecuperacaoSenha($conn, $email);
         } catch (Exception $e) {
-            $mysqli->rollback();
+            $conn->rollback();
             return "Erro ao atualizar a senha.";
         }
     }
 }
 
 // Executa a lógica principal
-$_SESSION['msg_rec'] = verificarEmail($mysqli, $email);
+$_SESSION['msg_rec'] = verificarEmail($conn, $email);
 exit;
 ?>
-
