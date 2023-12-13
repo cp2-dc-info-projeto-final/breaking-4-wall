4eea3673d3ff508d9c930b48e0f43fc01b12aba9
Alvaro1668 <95989290+Alvaro1668@users.noreply.github.com>
2023-12-13 20:22:21 -0300

    Update pesquisar.php

--- src/pesquisar.php
+++ src/pesquisar.php
@@ -8,6 +8,7 @@ $dbname = "CADASTRO";
 // Cria a conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
