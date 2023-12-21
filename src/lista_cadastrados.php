899fe55c79877b0099a85595e61e74112767a680
Alvaro1668 <95989290+Alvaro1668@users.noreply.github.com>
2023-12-20 22:11:29 -0300

    Update lista_cadastrados.php

--- src/lista_cadastrados.php
+++ src/lista_cadastrados.php
@@ -32,6 +32,62 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Usuários</title>
+     <style>
+      body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        .container {
+            background-color: #fff;
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        form {
+            display: inline-block;
+        }
+
+        button[type=submit] {
+            padding: 8px 15px;
+            border: none;
+            background-color: #5cb85c;
+            color: white;
+            font-size: 14px;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        button[type=submit]:hover {
+            background-color: #4cae4c;
+        }
+    </style>
     <link rel="stylesheet" href="estilo.css"> <!-- Incluído o link para o arquivo CSS -->
 </head>
 <body>
