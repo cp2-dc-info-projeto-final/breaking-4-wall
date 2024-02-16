eae41cfb6343155aed3be3eaf050c7e16c898988
Rodrigotccg4 <151775051+Rodrigotccg4@users.noreply.github.com>
2024-02-16 15:12:08 -0300

    Update processa_cadastro.php

--- src/processa_cadastro.php
+++ src/processa_cadastro.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 }
 
 // Verifique se o método POST foi usado (ou seja, o formulário foi enviado)
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
     // Receber dados do formulário
     $titulo = $_POST['titulo'];
     $anoLancamento = $_POST['anoLancamento'];
@@ -23,23 +23,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sinopse = $_POST['sinopse'];
     $avaliacaoImdb = $_POST['avaliacaoImdb'];
 
-    // Preparar o comando SQL
-    $stmt = $conn->prepare("INSERT INTO Filmes (Titulo, AnoLancamento, Diretor, Sinopse, AvaliacaoImdb) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssd", $titulo, $anoLancamento, $diretor, $sinopse, $avaliacaoImdb);
+   // Preparar o comando SQL
+$stmt = $conn->prepare("INSERT INTO Filmes (Titulo, AnoLancamento, Diretor, Sinopse, AvaliacaoImdb) VALUES (?, ?, ?, ?, ?)");
 
-    // Executar o comando SQL
-    if ($stmt->execute()) {
-        header("Location: dashboard.php");
-        echo "Filme cadastrado com sucesso!";
-    } else {
-        echo "Erro ao cadastrar o filme: " . $stmt->error;
-    }
-
-    // Fechar statement
-    $stmt->close();
+// Verificar se a preparação da consulta foi bem-sucedida
+if (!$stmt) {
+    die("Erro na preparação da consulta: " . $conn->error);
 }
+
+// Converter o ano de lançamento para um número decimal
+$anoLancamento = intval($_POST['anoLancamento']);
+
+// Bind dos parâmetros e seus tipos
+$stmt->bind_param("ssssd", $titulo, $anoLancamento, $diretor, $sinopse, $avaliacaoImdb);
+
+// Executar o comando SQL
+if ($stmt->execute()) {
+    header("Location: dashboard.php");
+    echo "Filme cadastrado com sucesso!";
+} else {
+    echo "Erro ao cadastrar o filme: " . $stmt->error;
+}
+
+// Fechar statement
+$stmt->close();
 
 // Fechar conexão
 $conn->close();
-
 ?>
